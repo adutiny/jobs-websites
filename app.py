8b2e43b956232f62454ef995646af4de7a32dbe7
woody1223 <113724666+adutiny@users.noreply.github.com>
2023-02-22 00:39:44 +0000

    added home page and custom page

--- app.py
+++ app.py
@@ -1,15 +1,20 @@
-from flask import Flask, render_template
-
+from flask import Flask,render_template
 
 # create Flask application
-app = Flask(__name__)
+#app = Flask(__name__)
+app=Flask(__name__,template_folder='Templates')
 
-
-# create route
 @app.route("/")
-def hello_world():
-    return "Hello, World"
+def home_page():
+    return render_template("home.html") 
+
+@app.route("/Custom Solutions")
+def custom_page():
+    return render_template("custom.html") 
 
 # run the application
 if __name__ == "__main__":
-    app.run(host= '0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', debug=True) 
+
+
+ 
